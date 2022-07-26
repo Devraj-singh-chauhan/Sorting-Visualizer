6f740a1c3fbf6f5da2db11c7e60d4f267b7bb770
Devraj <devrajsingh20002015@gmail.com>
2022-07-26 23:39:13 +0530

    edit SortingVisualizer.jsx and add bubble sort

--- src/Sorting Visualizer/SortingVisualizer.jsx
+++ src/Sorting Visualizer/SortingVisualizer.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {getMergeSortAnimations} from '../Sorting Algorithm/sortingAlgorithms.js';
+import {getBubbleSortAnimations} from '../Sorting Algorithm/bubbleSort.js';
+
 import './SortingVisualizer.css';
 
 // Change this value for the speed of the animations.
-const ANIMATION_SPEED_MS = 5;
+const ANIMATION_SPEED_MS = 1;
 
 // Change this value for the number of bars (value) in the array.
 const NUMBER_OF_ARRAY_BARS = 310;
@@ -59,6 +61,38 @@ export default class SortingVisualizer extends React.Component {
     }
   }
 
+  bubbleSort() {
+    const animations = getBubbleSortAnimations(this.state.array);
+    const arrayBars = document.getElementsByClassName('array-bar');
+    for (let i = 0; i < animations.length; i++) {
+      let barOneIdx, barTwoIdx;
+      
+      for(let k=0;k<2;k++,i++){
+        if(i%2!==1){
+          [barOneIdx, barTwoIdx]= animations[i];
+        }
+        //console.log(barOneIdx,barTwoIdx);
+        if (i%2!==1) {
+          const color = SECONDARY_COLOR;
+          setTimeout(() => {
+            arrayBars[barOneIdx].style.backgroundColor = color;
+            arrayBars[barTwoIdx].style.backgroundColor = color;
+          }, i * ANIMATION_SPEED_MS);
+        } 
+        else {
+          setTimeout(() => {
+            const [heightBar1,heightBar2] = animations[i];
+            arrayBars[barOneIdx].style.backgroundColor = PRIMARY_COLOR;
+            arrayBars[barTwoIdx].style.backgroundColor = PRIMARY_COLOR;
+            arrayBars[barOneIdx].style.height = `${heightBar1}px`;
+            arrayBars[barTwoIdx].style.height = `${heightBar2}px`;
+          }, i * ANIMATION_SPEED_MS);
+        }
+      }
+      i--;
+    }
+  }
+
   render() {
     const {array} = this.state;
 
@@ -72,6 +106,7 @@ export default class SortingVisualizer extends React.Component {
         ))}
         <button onClick={()=>this.resetArray()} class="btn btn-outline-primary">Reset Array</button>
         <button onClick={()=>this.mergeSort()} class="btn btn-outline-primary">Merge Sort</button>
+        <button onClick={()=>this.bubbleSort()} class="btn btn-outline-primary">Bubble Sort</button>
       </div>
       </>
     );
